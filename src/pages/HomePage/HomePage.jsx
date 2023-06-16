c87190c8157378b2ebd047be5a1e09e9afc1f7b4
Olena <newsroommy@gmail.com>
2023-06-16 14:53:21 +0300

    fix css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import css from './home-page.module.css';
 const HomePage = () => {
   return (
     <main>
-      <h1 className={css.title}>Follow to your star</h1>
+      <h1 className={css.title}>Follow your star</h1>
       <div className={css.homePageBlock}></div>
     </main>
   );
